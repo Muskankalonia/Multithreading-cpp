3773940de4e8ad12df913c99404ca120e2f04efc
Muskan <muskankalonia@gmail.com>
2026-03-15 08:57:12 +0000

    Some changes in bounded_producer_consumer.cpp

--- lld-codes/bounded_producer_consumer.cpp
+++ lld-codes/bounded_producer_consumer.cpp
@@ -9,6 +9,7 @@ class BlockingQueue {
         condition_variable not_full;
         condition_variable not_empty;
         mutex mtx;
+        bool closed = false;
 
     public :
         BlockingQueue(size_t cap) : max_size(cap) {}
@@ -22,20 +23,24 @@ class BlockingQueue {
             unique_lock<mutex> lock(mtx);
             
             not_full.wait(lock, [this]() {
-                return q.size() < max_size;
+                return q.size() < max_size || closed;
             });
             
+            if(closed) return; // for proper shutdown
+
             q.push(move(item));
             not_empty.notify_one();
         }
 
-        T pop() {
+        optional<T> pop() {
 
             unique_lock<mutex> lock(mtx);
 
             not_empty.wait(lock, [this](){
-                return q.size() != 0;
+                return !q.empty() || closed;
             });
+
+            if(q.empty()) return nullopt;
 
             T item = move(q.front()); // for high performance use move since the item can be very large this prevents unecessary copies
             q.pop();
@@ -44,27 +49,60 @@ class BlockingQueue {
             return item;
         }
 
+        void shutdown() {
+            {
+                unique_lock<mutex> lock(mtx);
+                closed = true;
+            }
+
+            not_empty.notify_all();
+            not_full.notify_all();
+        }
+
 };
 
 int main() {
     size_t cap = 5;
     BlockingQueue<int> q(cap);
 
-    thread producer([&q](){
-        for (int i = 0; i < 10; i++) {
-            q.push(i);
-            cout << "Pushed " << i << "\n";
-        }
-    });
+    vector<thread> producers, consumers;
 
-    thread consumer([&q](){
-        for (int i = 0; i < 10; i++) {
-            int item = q.pop();
-            cout << "Recieved " << item << "\n"; // don't use endl here because it is less efficient
-            this_thread::sleep_for(chrono::milliseconds(100));
-        }
-    });
+    for(int i = 0; i < 3; i++) {
 
-    producer.join(); consumer.join();
+        producers.emplace_back([&q](){
+
+            for (int i = 0; i < 10; i++) {
+                q.push(i);
+                string msg = "Pushed " + to_string(i) + "\n";
+                cout << msg;
+
+            }
+
+        });
+
+    }
+    
+    for(int i = 0; i < 3; i++) {
+
+        consumers.emplace_back([&q]() {
+
+            while(true) {
+
+                optional<int> item = q.pop();
+                if(!item.has_value()) break;
+                
+                string msg = "Recieved " + to_string(*item) + "\n";
+                cout << msg;
+
+            }
+
+        });
+    }
+    
+
+    for (auto &i : producers) i.join();
+    q.shutdown();
+
+    for (auto &i : consumers) i.join();
     return 0;
 }
